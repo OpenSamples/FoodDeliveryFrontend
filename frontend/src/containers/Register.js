75bc82f806dcdf608a26fa90af0fda76afdbeb7c
ivanab <ivana@opticaltone.com>
2021-02-21 00:04:16 +0100

    Fixed link, text and css

--- frontend/src/containers/Register.js
+++ frontend/src/containers/Register.js
@@ -1,59 +1,60 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 import { TextField, Button, Typography, IconButton } from "@material-ui/core";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
-import logo from '../assets/logo.png'
+import logo from "../assets/logo.png";
 
 const useStyles = makeStyles(() => ({
-    root: {
-        width: '100%',
-        minHeight: '90vh',
-        display: 'flex',
-        justifyContent: 'center'
-    },
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        marginTop: '7%',
-        border: '1px solid #ccc',
-        padding: '50px',
-        height: '50%',
-        paddingTop: '0'
-    },
-    input: {
-        margin: '15px 0'
-    },
-    button: {
-        margin: '20px 0'
-    },
-    a: {
-        textDecoration: 'none',
-        color: 'blue'
-    },
-    google: {
-        textDecoration: 'none',
-        alignSelf: 'center',
-        marginBottom: '20px',
-        backgroundColor: '#3f51b5',
-        color: '#fff',
-        padding: '10px'
-    },
-    textColor: {
-        color: 'rgba(0, 0, 0, 0.54)'
-    },
-    logo: {
-        width: '150px',
-        height: '100px',
-        alignSelf: 'center',
-        marginBottom: '20px'
-    },
-    homepage: {
-        margin: '10px',
-        color: 'rgba(0, 0, 0, 0.54)',
-        textDecoration: 'none'
-    }
-}))
+  root: {
+    width: "100%",
+    minHeight: "90vh",
+    display: "flex",
+    justifyContent: "center",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: "7%",
+    border: "1px solid #ccc",
+    padding: "50px",
+    height: "50%",
+    paddingTop: "0",
+    width: "35%",
+  },
+  input: {
+    margin: "15px 0",
+  },
+  button: {
+    margin: "20px 0",
+  },
+  a: {
+    textDecoration: "none",
+    color: "blue",
+  },
+  google: {
+    textDecoration: "none",
+    alignSelf: "center",
+    marginBottom: "20px",
+    backgroundColor: "#3f51b5",
+    color: "#fff",
+    padding: "10px",
+  },
+  textColor: {
+    color: "rgba(0, 0, 0, 0.54)",
+  },
+  logo: {
+    width: "150px",
+    height: "100px",
+    alignSelf: "center",
+    marginBottom: "20px",
+  },
+  homepage: {
+    margin: "10px",
+    color: "rgba(0, 0, 0, 0.54)",
+    textDecoration: "none",
+  },
+}));
 
 const Register = () => {
   const classes = useStyles();
@@ -105,14 +106,14 @@ const Register = () => {
             Register
           </Button>
           <Typography className={classes.textColor}>
-            Or sign in with...
+            Or Sign Up with...
           </Typography>
           <Link className={classes.google} to="/login-google">
             Google
           </Link>
           <Typography className={classes.textColor}>
             Already have an account?{" "}
-            <Link className={classes.a} to="/register">
+            <Link className={classes.a} to="/login">
               Sign-In
             </Link>
           </Typography>
